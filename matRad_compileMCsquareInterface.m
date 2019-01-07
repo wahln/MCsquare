86b494f04cc28f63100c38fb5e76825a859ed7df
Niklas Wahl <niklaswahl2011@gmail.com>
2019-01-07 05:48:44 +0100

    works on non-intel mex compilation

--- matRad_compileMCsquareInterface.m
+++ matRad_compileMCsquareInterface.m
@@ -1,11 +1,30 @@
-mex -setup % Setup the Intel compiler
+
 VCLIB = 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\amd64';   %path to visual studio library
 WINSDK = 'C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17763.0\um\x64';    %Windows SDK
 WINUCRT = 'C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17763.0\ucrt\x64'; %Windows UCRT
-INTEL65LIB = 'C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\compiler\lib\intel64';
+
 INTELMKLLIB = 'C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.2.187\windows\mkl\lib\intel64';
+
+%{
+% Intel Compiler with Visual studio
+mex -setup % Setup the Intel compiler
+
+
+INTEL64LIB = 'C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\compiler\lib\intel64';
+
 
 flags = '-Qopenmp -Qmkl -Qstd=c99 -MT';
 
-mex(['COMPFLAGS="$COMPFLAGS ' flags '"'],['-L"' INTELMKLLIB '"'],['-L"' VCLIB '"'],['-L"' INTEL64LIB '"'],['-L"' WINSDK '"'],['-L"' WINUCRT '"'],'matRad_mcSquareInterface.c',test{:})
+mex('-largeArrayDims',['COMPFLAGS="$COMPFLAGS ' flags '"'],['-L"' INTELMKLLIB '"'],['-L"' VCLIB '"'],['-L"' INTEL64LIB '"'],['-L"' WINSDK '"'],['-L"' WINUCRT '"'],'matRad_mcSquareInterface.c',test{:})
+%}
 
+% MSVC Only
+flags = '/MT /openmp'; %/std:c99';
+
+MKLINCLUDE = 'C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.2.187\windows\mkl\include';
+%INTELMKLLIB = 'C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.2.187\windows\mkl\lib\intel64_win';
+
+libs = {'-lmkl_intel_thread.lib','-lmkl_core.lib','-lmkl_intel_ilp64.lib'};%'-lmkl_intel_thread.lib','-lmkl_cdft_core.lib'};
+%'-lmkl_core.lib','-lmkl_intel_thread.lib','-lmkl_cdft_core.lib'
+
+mex('-largeArrayDims',['COMPFLAGS="$COMPFLAGS ' flags '"'],['-I"' MKLINCLUDE '"'],['-L"' INTELMKLLIB '"'],libs{:},['-L"' VCLIB '"'],['-L"' WINSDK '"'],['-L"' WINUCRT '"'],'matRad_mcSquareInterface.c',test{:})
